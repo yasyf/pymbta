335a2b62475536280456aa8d51da27c273289386
Yasyf Mohamedali <yasyfm@gmail.com>
2014-06-05 01:50:07 -0400

    added bootstrap, more accurate train info

--- webapp.py
+++ webapp.py
@@ -23,6 +23,20 @@ def next_trains():
 	else:
 		return jsonify({'route_name': 'Nothing!'})
 
+@app.route('/api/nearby_stop', methods=['GET', 'POST'])
+def nearby_stops():
+	lat = request.values.get('lat')
+	lon = request.values.get('lon')
+	dt = request.values.get('dt')
+	direction = request.values.get('direction')
+	line = request.values.get('line')
+	
+	nearest_stop = client.closest_stop((lat, lon), _datetime=dt, direction=direction, line=line)
+	if nearest_stop:
+		return jsonify(nearest_stop)
+	else:
+		return jsonify({'route_name': 'Nothing!'})
+
 if __name__ == '__main__':
 	if os.environ.get('PORT'):
 		app.run(host='0.0.0.0',port=int(os.environ.get('PORT')),debug=False)
